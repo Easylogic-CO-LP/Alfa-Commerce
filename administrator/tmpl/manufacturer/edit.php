8e0682220a48d670b3605cf0b0d448871fb57b3d
Agoritsa <94460605+Agoritsa@users.noreply.github.com>
2024-08-08 12:52:08 +0300

    Update edit.php

--- administrator/tmpl/manufacturer/edit.php
+++ administrator/tmpl/manufacturer/edit.php
@@ -28,6 +28,7 @@ HTMLHelper::_('bootstrap.tooltip');
 
 	
 				<?php echo $this->form->renderField('name'); ?>
+				<?php echo $this->form->renderField('alias'); ?>
 	<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'manufacturer')); ?>
 	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'manufacturer', Text::_('COM_ALFA_TAB_MANUFACTURER', true)); ?>
 	<div class="row-fluid">
@@ -36,11 +37,21 @@ HTMLHelper::_('bootstrap.tooltip');
 				<legend><?php echo Text::_('COM_ALFA_FIELDSET_MANUFACTURER'); ?></legend>
 				<?php echo $this->form->renderField('id'); ?>
 				<?php echo $this->form->renderField('state'); ?>
-				<?php echo $this->form->renderField('alias'); ?>
 				<?php echo $this->form->renderField('desc'); ?>
+				<?php echo $this->form->renderField('website'); ?>
+				<?php if ($this->state->params->get('save_history', 1)) : ?>
+				<?php endif; ?>
+			</fieldset>
+		</div>
+	</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'SearchEngines', Text::_('COM_ALFA_TAB_SEARCHENGINES', true)); ?>
+	<div class="row-fluid">
+		<div class="col-md-12 form-horizontal">
+			<fieldset class="adminform">
+				<legend><?php echo Text::_('COM_ALFA_FIELDSET_SEARCHENGINES'); ?></legend>
 				<?php echo $this->form->renderField('meta_title'); ?>
 				<?php echo $this->form->renderField('meta_desc'); ?>
-				<?php echo $this->form->renderField('website'); ?>
 				<?php if ($this->state->params->get('save_history', 1)) : ?>
 					<div class="control-group">
 						<div class="control-label"><?php echo $this->form->getLabel('version_note'); ?></div>
@@ -50,7 +61,7 @@ HTMLHelper::_('bootstrap.tooltip');
 			</fieldset>
 		</div>
 	</div>
-	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+	
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
