cff910e1ff5603a984ab9bda844a1846751e6dae
Agamemnon Fakas <96840280+niosme@users.noreply.github.com>
2024-08-08 14:51:46 +0300

    Update edit.php

--- administrator/tmpl/manufacturer/edit.php
+++ administrator/tmpl/manufacturer/edit.php
@@ -22,19 +22,18 @@ $wa->useScript('keepalive')
 HTMLHelper::_('bootstrap.tooltip');
 ?>
 
-
 <form
 	action="<?php echo Route::_('index.php?option=com_alfa&layout=edit&id=' . (int) $this->item->id); ?>"
 	method="post" enctype="multipart/form-data" name="adminForm" id="manufacturer-form" class="form-validate form-horizontal">
 
-	  	  <div class="row name-alias form-vertical mb-3">
-			   <div class="col-12 col-md-6">
-			       <?php echo $this->form->renderField('name'); ?>
-			   </div>
-			   <div class="col-12 col-md-6">
-			       <?php echo $this->form->renderField('alias'); ?>
-			   </div>
-		   </div>
+				<div class="row name-alias form-vertical mb-3">
+					<div class="col-12 col-md-6">
+					   <?php echo $this->form->renderField('name'); ?>
+					</div>
+					<div class="col-12 col-md-6">
+					   <?php echo $this->form->renderField('alias'); ?>
+					</div>
+				</div>
 
 	<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'manufacturer')); ?>
 	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'manufacturer', Text::_('COM_ALFA_TAB_MANUFACTURER', true)); ?>
@@ -45,20 +44,9 @@ HTMLHelper::_('bootstrap.tooltip');
 				<?php echo $this->form->renderField('id'); ?>
 				<?php echo $this->form->renderField('state'); ?>
 				<?php echo $this->form->renderField('desc'); ?>
-				<?php echo $this->form->renderField('website'); ?>
-				<?php if ($this->state->params->get('save_history', 1)) : ?>
-				<?php endif; ?>
-			</fieldset>
-		</div>
-	</div>
-	<?php echo HTMLHelper::_('uitab.endTab'); ?>
-	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'SearchEngines', Text::_('COM_ALFA_TAB_SEARCHENGINES', true)); ?>
-	<div class="row-fluid">
-		<div class="col-md-12 form-horizontal">
-			<fieldset class="adminform">
-				<legend><?php echo Text::_('COM_ALFA_FIELDSET_SEARCHENGINES'); ?></legend>
 				<?php echo $this->form->renderField('meta_title'); ?>
 				<?php echo $this->form->renderField('meta_desc'); ?>
+				<?php echo $this->form->renderField('website'); ?>
 				<?php if ($this->state->params->get('save_history', 1)) : ?>
 					<div class="control-group">
 						<div class="control-label"><?php echo $this->form->getLabel('version_note'); ?></div>
@@ -68,21 +56,17 @@ HTMLHelper::_('bootstrap.tooltip');
 			</fieldset>
 		</div>
 	</div>
-	
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
+
 	<?php echo $this->form->renderField('created_by'); ?>
 	<?php echo $this->form->renderField('modified_by'); ?>
-	<?php echo HTMLHelper::_('uitab.endTab'); ?>
-	
+
 	<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 
 	<input type="hidden" name="task" value=""/>
-	<?php echo HTMLHelper::_('form.token'); ?>
-
-</form>
-
 	<?php echo HTMLHelper::_('form.token'); ?>
 
 </form>
