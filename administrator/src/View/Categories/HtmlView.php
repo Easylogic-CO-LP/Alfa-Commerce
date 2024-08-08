651342a753153fb34123ca8b3a764ba6cbed2706
Beat00 <127697643+Beat00@users.noreply.github.com>
2024-08-08 15:27:50 +0300

    Alex categories backend (#21)
    * Categories backend progress
    
    Προς το παρόν δουλεύει τουλάχιστον.
    
    * Update CategoriesField.php
    
    * Update HtmlView.php

--- administrator/src/View/Categories/HtmlView.php
+++ administrator/src/View/Categories/HtmlView.php
@@ -19,6 +19,7 @@ use \Joomla\CMS\Language\Text;
 use \Joomla\Component\Content\Administrator\Extension\ContentComponent;
 use \Joomla\CMS\Form\Form;
 use \Joomla\CMS\HTML\Helpers\Sidebar;
+
 /**
  * View class for a list of Categories.
  *
@@ -26,159 +27,190 @@ use \Joomla\CMS\HTML\Helpers\Sidebar;
  */
 class HtmlView extends BaseHtmlView
 {
-	protected $items;
+    protected $items;
 
-	protected $pagination;
+    protected $pagination;
 
-	protected $state;
+    protected $state;
 
-	/**
-	 * Display the view
-	 *
-	 * @param   string  $tpl  Template name
-	 *
-	 * @return void
-	 *
-	 * @throws Exception
-	 */
-	public function display($tpl = null)
-	{
-		$this->state = $this->get('State');
-		$this->items = $this->get('Items');
-		$this->pagination = $this->get('Pagination');
-		$this->filterForm = $this->get('FilterForm');
-		$this->activeFilters = $this->get('ActiveFilters');
+    protected $sortedNames = array();
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			throw new \Exception(implode("\n", $errors));
-		}
+    protected $nestedNames = array();
 
-		$this->addToolbar();
+    /**
+     * Display the view
+     *
+     * @param string $tpl Template name
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function display($tpl = null)
+    {
+        $this->state = $this->get('State');
+        $this->items = $this->get('Items');
+        $this->pagination = $this->get('Pagination');
+        $this->filterForm = $this->get('FilterForm');
+        $this->activeFilters = $this->get('ActiveFilters');
 
-		$this->sidebar = Sidebar::render();
-		parent::display($tpl);
-	}
+        // Check for errors.
+        if (count($errors = $this->get('Errors'))) {
+            throw new \Exception(implode("\n", $errors));
+        }
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.0.1
-	 */
-	protected function addToolbar()
-	{
-		$state = $this->get('State');
-		$canDo = AlfaHelper::getActions();
+        // Modify names for nested categories
+        // Create associative array based on names
+        $nestedCategoryNames = AlfaHelper::buildNestedArray($this->items);
+        AlfaHelper::iterateNestedArray($nestedCategoryNames, function ($node, $fullPath) {
+            $this->sortedNames[] = $node->name; // Collect names
+            $this->nestedNames[$node->name] = $fullPath; // Map names to full paths
+        }, false);
 
-		ToolbarHelper::title(Text::_('COM_ALFA_TITLE_CATEGORIES'), "generic");
+        // Create a mapping from names to objects
+        $nameToObjectMap = [];
+        foreach ($this->items as $item) {
+            $nameToObjectMap[$item->name] = $item;
+        }
 
-		$toolbar = Toolbar::getInstance('toolbar');
+        // Create a new array based on the sorted names
+        $sortedItems = [];
+        foreach ($this->sortedNames as $name) {
+            if (isset($nameToObjectMap[$name])) {
+                $sortedItems[] = $nameToObjectMap[$name];
+            }
+        }
 
-		// Check if the form exists before showing the add/edit buttons
-		$formPath = JPATH_COMPONENT_ADMINISTRATOR . '/src/View/Categories';
+        // Map sorted items to include full path as names
+        $fullPathNames = [];
+        foreach ($this->sortedNames as $name) {
+            if (isset($this->nestedNames[$name])) {
+                $fullPathNames[$name] = $this->nestedNames[$name];
+            }
+        }
 
-		if (file_exists($formPath))
-		{
-			if ($canDo->get('core.create'))
-			{
-				$toolbar->addNew('category.add');
-			}
-		}
+        // Update names in sorted items with full paths
+        foreach ($sortedItems as $item) {
+            $item->name = $fullPathNames[$item->name];
+        }
 
-		if ($canDo->get('core.edit.state'))
-		{
-			$dropdown = $toolbar->dropdownButton('status-group')
-				->text('JTOOLBAR_CHANGE_STATUS')
-				->toggleSplit(false)
-				->icon('fas fa-ellipsis-h')
-				->buttonClass('btn btn-action')
-				->listCheck(true);
+        // Assign sorted items back
+        $this->items = $sortedItems;
 
-			$childBar = $dropdown->getChildToolbar();
+        $this->addToolbar();
 
-			if (isset($this->items[0]->state))
-			{
-				$childBar->publish('categories.publish')->listCheck(true);
-				$childBar->unpublish('categories.unpublish')->listCheck(true);
-				$childBar->archive('categories.archive')->listCheck(true);
-			}
-			elseif (isset($this->items[0]))
-			{
-				// If this component does not use state then show a direct delete button as we can not trash
-				$toolbar->delete('categories.delete')
-				->text('JTOOLBAR_EMPTY_TRASH')
-				->message('JGLOBAL_CONFIRM_DELETE')
-				->listCheck(true);
-			}
+        $this->sidebar = Sidebar::render();
+        parent::display($tpl);
+    }
 
-			$childBar->standardButton('duplicate')
-				->text('JTOOLBAR_DUPLICATE')
-				->icon('fas fa-copy')
-				->task('categories.duplicate')
-				->listCheck(true);
+    /**
+     * Add the page title and toolbar.
+     *
+     * @return  void
+     *
+     * @since   1.0.1
+     */
+    protected function addToolbar()
+    {
+        $state = $this->get('State');
+        $canDo = AlfaHelper::getActions();
 
-			if (isset($this->items[0]->checked_out))
-			{
-				$childBar->checkin('categories.checkin')->listCheck(true);
-			}
+        ToolbarHelper::title(Text::_('COM_ALFA_TITLE_CATEGORIES'), "generic");
 
-			if (isset($this->items[0]->state))
-			{
-				$childBar->trash('categories.trash')->listCheck(true);
-			}
-		}
+        $toolbar = Toolbar::getInstance('toolbar');
 
-		
+        // Check if the form exists before showing the add/edit buttons
+        $formPath = JPATH_COMPONENT_ADMINISTRATOR . '/src/View/Categories';
 
-		// Show trash and delete for components that uses the state field
-		if (isset($this->items[0]->state))
-		{
+        if (file_exists($formPath)) {
+            if ($canDo->get('core.create')) {
+                $toolbar->addNew('category.add');
+            }
+        }
 
-			if ($this->state->get('filter.state') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete'))
-			{
-				$toolbar->delete('categories.delete')
-					->text('JTOOLBAR_EMPTY_TRASH')
-					->message('JGLOBAL_CONFIRM_DELETE')
-					->listCheck(true);
-			}
-		}
+        if ($canDo->get('core.edit.state')) {
+            $dropdown = $toolbar->dropdownButton('status-group')
+                ->text('JTOOLBAR_CHANGE_STATUS')
+                ->toggleSplit(false)
+                ->icon('fas fa-ellipsis-h')
+                ->buttonClass('btn btn-action')
+                ->listCheck(true);
 
-		if ($canDo->get('core.admin'))
-		{
-			$toolbar->preferences('com_alfa');
-		}
+            $childBar = $dropdown->getChildToolbar();
 
-		// Set sidebar action
-		Sidebar::setAction('index.php?option=com_alfa&view=categories');
-	}
-	
-	/**
-	 * Method to order fields 
-	 *
-	 * @return void 
-	 */
-	protected function getSortFields()
-	{
-		return array(
-			'a.`ordering`' => Text::_('JGRID_HEADING_ORDERING'),
-			'a.`id`' => Text::_('JGRID_HEADING_ID'),
-			'a.`name`' => Text::_('COM_ALFA_CATEGORIES_NAME'),
-			'a.`state`' => Text::_('JSTATUS'),
-		);
-	}
+            if (isset($this->items[0]->state)) {
+                $childBar->publish('categories.publish')->listCheck(true);
+                $childBar->unpublish('categories.unpublish')->listCheck(true);
+                $childBar->archive('categories.archive')->listCheck(true);
+            } elseif (isset($this->items[0])) {
+                // If this component does not use state then show a direct delete button as we can not trash
+                $toolbar->delete('categories.delete')
+                    ->text('JTOOLBAR_EMPTY_TRASH')
+                    ->message('JGLOBAL_CONFIRM_DELETE')
+                    ->listCheck(true);
+            }
 
-	/**
-	 * Check if state is set
-	 *
-	 * @param   mixed  $state  State
-	 *
-	 * @return bool
-	 */
-	public function getState($state)
-	{
-		return isset($this->state->{$state}) ? $this->state->{$state} : false;
-	}
+            $childBar->standardButton('duplicate')
+                ->text('JTOOLBAR_DUPLICATE')
+                ->icon('fas fa-copy')
+                ->task('categories.duplicate')
+                ->listCheck(true);
+
+            if (isset($this->items[0]->checked_out)) {
+                $childBar->checkin('categories.checkin')->listCheck(true);
+            }
+
+            if (isset($this->items[0]->state)) {
+                $childBar->trash('categories.trash')->listCheck(true);
+            }
+        }
+
+
+        // Show trash and delete for components that uses the state field
+        if (isset($this->items[0]->state)) {
+
+            if ($this->state->get('filter.state') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete')) {
+                $toolbar->delete('categories.delete')
+                    ->text('JTOOLBAR_EMPTY_TRASH')
+                    ->message('JGLOBAL_CONFIRM_DELETE')
+                    ->listCheck(true);
+            }
+        }
+
+        if ($canDo->get('core.admin')) {
+            $toolbar->preferences('com_alfa');
+        }
+
+        // Set sidebar action
+        Sidebar::setAction('index.php?option=com_alfa&view=categories');
+    }
+
+    /**
+     * Method to order fields
+     *
+     * @return void
+     */
+    protected
+    function getSortFields()
+    {
+        return array(
+            'a.`ordering`' => Text::_('JGRID_HEADING_ORDERING'),
+            'a.`id`' => Text::_('JGRID_HEADING_ID'),
+            'a.`name`' => Text::_('COM_ALFA_CATEGORIES_NAME'),
+            'a.`state`' => Text::_('JSTATUS'),
+        );
+    }
+
+    /**
+     * Check if state is set
+     *
+     * @param mixed $state State
+     *
+     * @return bool
+     */
+    public
+    function getState($state)
+    {
+        return isset($this->state->{$state}) ? $this->state->{$state} : false;
+    }
 }
