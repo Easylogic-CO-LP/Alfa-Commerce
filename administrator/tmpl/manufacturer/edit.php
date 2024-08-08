79c72f5ca5008f324497f8c72379e77a066c0e02
Agoritsa <94460605+Agoritsa@users.noreply.github.com>
2024-08-08 14:58:36 +0300

    Update edit.php (#18)
    * Update edit.php
    
    * Update edit.php
    
    * Update manufacturer.xml
    
    * Update edit.php
    
    ξέχασες αυτό
    
    <?php echo HTMLHelper::_('uitab.endTab'); ?>
    
    * Update edit.php
    
    * Update edit.php
    
    * Update edit.php
    
    ---------
    
    Co-authored-by: Agamemnon Fakas <96840280+niosme@users.noreply.github.com>

--- administrator/tmpl/manufacturer/edit.php
+++ administrator/tmpl/manufacturer/edit.php
@@ -26,38 +26,58 @@ HTMLHelper::_('bootstrap.tooltip');
 	action="<?php echo Route::_('index.php?option=com_alfa&layout=edit&id=' . (int) $this->item->id); ?>"
 	method="post" enctype="multipart/form-data" name="adminForm" id="manufacturer-form" class="form-validate form-horizontal">
 
-	
-				<?php echo $this->form->renderField('name'); ?>
+				<div class="row name-alias form-vertical mb-3">
+					<div class="col-12 col-md-6">
+					   <?php echo $this->form->renderField('name'); ?>
+					</div>
+					<div class="col-12 col-md-6">
+					   <?php echo $this->form->renderField('alias'); ?>
+					</div>
+				</div>
+
+
+
 	<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'manufacturer')); ?>
 	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'manufacturer', Text::_('COM_ALFA_TAB_MANUFACTURER', true)); ?>
 	<div class="row-fluid">
 		<div class="col-md-12 form-horizontal">
 			<fieldset class="adminform">
-				<legend><?php echo Text::_('COM_ALFA_FIELDSET_MANUFACTURER'); ?></legend>
-				<?php echo $this->form->renderField('id'); ?>
-				<?php echo $this->form->renderField('state'); ?>
-				<?php echo $this->form->renderField('alias'); ?>
 				<?php echo $this->form->renderField('desc'); ?>
-				<?php echo $this->form->renderField('meta_title'); ?>
-				<?php echo $this->form->renderField('meta_desc'); ?>
+				
 				<?php echo $this->form->renderField('website'); ?>
+
+				<?php echo $this->form->renderField('state'); ?>
+
 				<?php if ($this->state->params->get('save_history', 1)) : ?>
 					<div class="control-group">
 						<div class="control-label"><?php echo $this->form->getLabel('version_note'); ?></div>
 						<div class="controls"><?php echo $this->form->getInput('version_note'); ?></div>
 					</div>
 				<?php endif; ?>
+				<?php echo $this->form->renderField('id'); ?>
 			</fieldset>
 		</div>
 	</div>
 	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'SearchEngines', Text::_('COM_ALFA_TAB_SEARCHENGINES', true)); ?>
+	<div class="row-fluid">
+		<div class="col-md-12 form-horizontal">
+			<fieldset class="adminform">
+				<?php echo $this->form->renderField('meta_title'); ?>
+				<?php echo $this->form->renderField('meta_desc'); ?>
+			</fieldset>
+		</div>
+	</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
+
 	<?php echo $this->form->renderField('created_by'); ?>
 	<?php echo $this->form->renderField('modified_by'); ?>
 
-	
 	<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 
 	<input type="hidden" name="task" value=""/>
