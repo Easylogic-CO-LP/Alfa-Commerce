cd6eab8f0779e501b1be61f21c73b4130b879e28
Agamemnon Fakas <96840280+niosme@users.noreply.github.com>
2024-08-09 13:11:36 +0300

    Categories Logic (#22)
    * Update ItemModel.php
    
    * Update script.php
    
    * Update install.mysql.utf8.sql
    
    * Update install.mysql.utf8.sql
    
    * Categories Logic

--- administrator/src/Field/CategoriesField.php
+++ administrator/src/Field/CategoriesField.php
@@ -27,37 +27,50 @@ use Joomla\CMS\HTML\HTMLHelper;
 class CategoriesField extends ListField
 {
 
-    protected $type = 'categories';
+	protected $type = 'categories';
 
-    protected $options = [];
+	protected $options = [];
 
-    protected function getOptions()
-    {
-        $app = Factory::getApplication();
-        $component = $app->bootComponent('com_alfa');
-        $factory = $component->getMVCFactory();
-        $model = $factory->createModel('Categories', 'Administrator');
-        $model->setState('filter.state', '*');
-        $categories = $model->getItems();// Fetch the item using the model's getItem method.
+	protected function getOptions()
+	{
+		$removeCurrent = $this->element['removeCurrent']=='true' ? true : false;
+		$showPath      = $this->element['showPath']=='true' ? true : false;
 
-        $nestedCategories = AlfaHelper::buildNestedArray($categories);
-        AlfaHelper::iterateNestedArray($nestedCategories, function ($node, $fullPath) {
-            $this->options['cat-' . $node->id] = array('value' => $node->id, 'text' => $fullPath);
-        }, false);
+		$this->options = parent::getOptions();
 
-        if ($this->element['removeCurrent'] == "true") {
-            $idToRemove = $this->form->getData()->get('id');
-            unset($this->options['cat-' . $idToRemove]);
-        }
-        return array_merge(parent::getOptions(), $this->options);
-    }
+		$app       = Factory::getApplication();
+		$component = $app->bootComponent('com_alfa');
+		$factory   = $component->getMVCFactory();
+		$model     = $factory->createModel('Categories', 'Administrator');
+		$model->setState('filter.state', '*');
 
-    public function getAttribute($attr_name, $default = null)
-    {
-        // if (!empty($this->element[$attr_name])) {
-        //     return $this->element[$attr_name];
-        // } else {
-        //     return $default;
-        // }
-    }
+		//		TODO: set filtering by name ( is not setting )
+		//	    $app->input->set('list.ordering', 'name');
+		//	    $app->input->set('list.direction', 'DESC');
+		//	    $model->setState('list.ordering', 'name');
+		//	    $model->setState('list.direction', 'ASC');
+
+		$categories = $model->getItems();
+
+		foreach ($categories as $category)
+		{
+			$this->options['cat-' . $category->id] =
+				array('value' => $category->id,
+				      'text'  => ($showPath
+					                ?$category->path
+					                :str_repeat('-', $category->depth).$category->name
+				                )
+	                );
+	    }
+
+		if ($removeCurrent)
+		{
+			$idToRemove = $this->form->getData()->get('id');
+			unset($this->options['cat-' . $idToRemove]);
+		}
+
+		return $this->options;
+
+	}
+
 }
