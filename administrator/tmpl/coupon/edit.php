541de783babc889f5903f5114cf1728d5cdbb198
Alex K <127697643+akapno@users.noreply.github.com>
2024-08-21 15:16:51 +0300

    Coupon progress 1

--- administrator/tmpl/coupon/edit.php
+++ administrator/tmpl/coupon/edit.php
@@ -20,6 +20,9 @@ $wa = $this->document->getWebAssetManager();
 $wa->useScript('keepalive')
 	->useScript('form.validate');
 HTMLHelper::_('bootstrap.tooltip');
+
+$input = Factory::getApplication()->getInput();
+
 ?>
 
 <form
@@ -40,8 +43,6 @@ HTMLHelper::_('bootstrap.tooltip');
 				<?php echo $this->form->renderField('min_value'); ?>
 				<?php echo $this->form->renderField('max_value'); ?>
 				<?php echo $this->form->renderField('hidden'); ?>
-				<?php echo $this->form->renderField('start_date'); ?>
-				<?php echo $this->form->renderField('end_date'); ?>
 				<?php echo $this->form->renderField('associate_to_new_users'); ?>
 				<?php echo $this->form->renderField('user_associated'); ?>
 			</fieldset>
@@ -53,13 +54,53 @@ HTMLHelper::_('bootstrap.tooltip');
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
-	<?php echo $this->form->renderField('created_by'); ?>
-	<?php echo $this->form->renderField('modified_by'); ?>
 
 	
-	<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 
-	<input type="hidden" name="task" value=""/>
+    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('COM_ALFA_FIELDSET_PUBLISHING_SEO')); ?>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <fieldset id="fieldset-publishingdata" class="options-form">
+                <legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
+                <div>
+                    <?php echo $this->form->renderField('publish_up'); ?>
+
+                    <?php echo $this->form->renderField('publish_down'); ?>
+
+                    <?php echo $this->form->renderField('allowedUsers'); ?>
+
+                    <?php echo $this->form->renderField('allowedUserGroups'); ?>
+
+                    <?php echo $this->form->renderField('created_by'); ?>
+
+                    <?php echo $this->form->renderField('modified'); ?>
+
+                    <?php echo $this->form->renderField('modified_by'); ?>
+
+                    <?php echo $this->form->renderField('id'); ?>
+
+
+                </div>
+            </fieldset>
+        </div>
+        <div class="col-12 col-lg-6">
+            <fieldset id="fieldset-metadata" class="options-form">
+                <legend><?php echo Text::_('JGLOBAL_FIELDSET_METADATA_OPTIONS'); ?></legend>
+                <div>
+                    <?php echo $this->form->renderField('meta_title'); ?>
+                    <?php echo $this->form->renderField('meta_desc'); ?>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+
+    <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+
+    <input type="hidden" name="task" value="">
+    <input type="hidden" name="return" value="<?php echo $input->getBase64('return'); ?>">
+
 	<?php echo HTMLHelper::_('form.token'); ?>
 
 </form>
