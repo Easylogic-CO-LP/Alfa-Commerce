7c08f36e2027ad421294a4d01ae6e0a704097569
Agamemnon Fakas <96840280+niosme@users.noreply.github.com>
2024-08-27 11:09:12 +0300

    Update install.mysql.utf8.sql
    Fixed sql

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -65,8 +65,6 @@ CREATE TABLE IF NOT EXISTS `#__alfa_items` (
 `checked_out` INT(11)  UNSIGNED,
 `checked_out_time` DATETIME NULL  DEFAULT NULL ,
 `created_by` INT(11)  NULL  DEFAULT 0,
-`publish_up` datetime DEFAULT NULL,
-`publish_down` datetime DEFAULT NULL,
 `modified` datetime NOT NULL,
 `modified_by` INT(11)  NULL  DEFAULT 0,
 `ordering` INT(11)  NULL  DEFAULT 0,
@@ -122,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `#__alfa_items_prices` (
     KEY `idx_currency_id` (`currency_id`),
     KEY `idx_usergroup_id` (`usergroup_id`),
     KEY `idx_user_id` (`user_id`),
-    KEY `idx_country_id` (`country_id`),
+    KEY `idx_country_id` (`country_id`)
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 
